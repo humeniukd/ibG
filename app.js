d815e9bb878bb6bfc84fefd9b754dcc199cb5571
lou <some@example.com>
2016-01-21 16:17:07 +0200

    queue TODO: get data by chunks

--- app.js
+++ app.js
@@ -53,27 +53,27 @@
         player = null;
     })
 
-    //socket.on('load-track', function (trackData) {
-    //    if (trackData.length === 0) {
-    //        return;
-    //    }
-    //    player.load(trackData);
-    //    console.log(player.getQueue().getLength());
-    //});
-    function start() {
-        //socket.emit('load-track', {
-        //    appId: id,
-        //    startTimeStamp: fromDateTime,
-        //    endTimeStamp: toDateTime
-        //});
-        for(var n = 0; n < 7; n++){
-            $.get('td'+n+'.json', function (trackData) {
-                if (trackData.length === 0) {
-                    return;
-                }
-                player.load(trackData);
-                console.log(player.getQueue().getLength());
-            });
+    socket.on('load-track', function (trackData) {
+        if (trackData.length === 0) {
+            return;
         }
+        player.load(trackData);
+        console.log(player.getQueue().getLength());
+    });
+    function start() {
+        socket.emit('load-track', {
+            appId: id,
+            startTimeStamp: fromDateTime,
+            endTimeStamp: toDateTime
+        });
+        //for(var n = 0; n < 7; n++){
+        //    $.get('td'+n+'.json', function (trackData) {
+        //        if (trackData.length === 0) {
+        //            return;
+        //        }
+        //        player.load(trackData);
+        //        console.log(player.getQueue().getLength());
+        //    });
+        //}
     }
 }(jQuery))
